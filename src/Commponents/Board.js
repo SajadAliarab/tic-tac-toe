369597d82ccde03b14768edf518597761d3f1dad
Sajad Aliarab <sajad.aliarab@gmail.com>
2023-05-04 17:36:20 +0100

    3 update 4th MAY

--- src/Commponents/Board.js
+++ src/Commponents/Board.js
@@ -2,23 +2,34 @@ import React, {useState} from 'react';
 import Square from './Square';
 
 const Board = () => {
+    const[xIsNext,setIsNext] = useState(true);
     const[square , setSquare]= useState(Array(9).fill(null));
+    const handleClick= (i) => {
+        const nextSquare=square.slice();
+        if(xIsNext){
+            nextSquare[i]="X";
+        }else{
+            nextSquare[i]="O"
+        }
+        setSquare(nextSquare);
+        setIsNext(!xIsNext);
+    }
     return ( 
         <div>
             <div>
-                <Square />
-                <Square/>
-                <Square/>
+                <Square value={square[0]} onSquareClick={()=> handleClick(0)}/>
+                <Square value={square[1]} onSquareClick={()=> handleClick(1)}/>
+                <Square value={square[2]} onSquareClick={()=> handleClick(2)}/>
             </div>
             <div>
-                <Square/>
-                <Square/>
-                <Square/>
+                <Square value={square[3]} onSquareClick={()=> handleClick(3)}/>
+                <Square value={square[4]} onSquareClick={()=> handleClick(4)}/>
+                <Square value={square[5]} onSquareClick={()=> handleClick(5)}/>
             </div>
             <div>
-                <Square/>
-                <Square />
-                <Square/>
+                <Square value={square[6]} onSquareClick={()=> handleClick(6)}/>
+                <Square value={square[7]} onSquareClick={()=> handleClick(7)}/>
+                <Square value={square[8]} onSquareClick={()=> handleClick(8)}/>
             </div>
         </div>
      );
--- src/Commponents/Square.js
+++ src/Commponents/Square.js
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
 
-const Square = () => {
-    const [value , setValue]= useState(null);
-    const handleClick= () => {
-        setValue('X')
-    }
+const Square = ({value , onSquareClick}) => {
+
     return (
     
-            <button className='square' style={{width:"50px" , height:"50px" , fontSize:"30px"}} onClick={handleClick}>{value}</button>
+            <button className='square' style={{width:"50px" , height:"50px" , fontSize:"30px"}} onClick={onSquareClick}>{value}</button>
      );
 }
  
